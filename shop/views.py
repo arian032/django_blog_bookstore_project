5621bd761c8818f553dbe5d08cdb431fafda02c1
arian <arianroben7@gmail.com>
2022-12-22 20:40:24 +0330

    add shop models, urls, templates and views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'shop/product_list.html'
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'shop/product_detail.html'
+
+
+class ProductCreateView(generic.CreateView):
+    model = Product
+    fields = ['title', 'description', 'price', 'active']
+    template_name = 'shop/product_create.html'
+
+
+class ProductUpdateView(generic.UpdateView):
+    model = Product
+    fields = ['title', 'description', 'price', 'active']
+    template_name = 'shop/product_update.html'
+
+
+class ProductDeleteView(generic.DeleteView):
+    model = Product
+    template_name = 'shop/product_delete.html'
+
