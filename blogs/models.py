207bdcaca142bc50a3e144cfadaa3207ac8be829
arian <arianroben7@gmail.com>
2022-12-23 22:15:04 +0330

    add BlogPost and Comment model for blog app

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    author = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
